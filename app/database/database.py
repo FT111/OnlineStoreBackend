4be8b407ef2dbe83525f84c0746e987f63e7eb3b
FT111 <thetaylor2016@gmail.com>
2024-07-27 23:50:06 +0100

    Started search system

--- app/database/database.py
+++ app/database/database.py
@@ -1,11 +1,14 @@
-
 import sqlite3
 
 
 # Function to get the database connection - A dependency for DB operations
+# Yields the connection and will always close it after a request is done
 def getDBSession() -> sqlite3.Connection:
     conn = sqlite3.connect('databaseProd.db')
     conn.row_factory = sqlite3.Row
-    return conn
 
-getDBSession()
+    try:
+        yield conn
+    finally:
+        conn.close()
+
